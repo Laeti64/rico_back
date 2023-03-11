6b6f9dcf69013d1fc61ffd82f5d552b39b3e0a94
Pupuce-64 <laeti.decandt@gmail.com>
2023-03-11 18:46:22 +0100

    delete and update ok

--- src/api/tickets/routes.ts
+++ src/api/tickets/routes.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.post("/create", ticketController.create);
 router.get("/", ticketController.getAll);
 router.get("/:id", ticketController.getOne);
+router.delete("/:id", ticketController.delete);
+router.post("/:id", ticketController.update);
 
 export default router;
